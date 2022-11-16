1d10699dff663b67dde21364e2cfafcec3941976
Carlos Salazar <crslarez@gmail.com>
2022-11-16 00:14:01 -0500

    db-consumo-API-js-listo

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const URL_API = "http://localhost:3000/peliculas";
-export const getPeliculas = async () => {
+const API_URL = "http://localhost:3000/peliculas";
+
+const getMovie = async () => {
   try {
-    const peticion = await fetch(URL_API);
-    const parsear = await peticion.json();
-    console.log(parsear);
-    return parsear;
+    const response = (await fetch(API_URL)).json();
+    return response;
   } catch (error) {
     console.log(error);
   }
 };
+
+function createCard(movie) {
+  const containerCards = document.getElementById("container-cards");
+  const cards = /*html*/ `
+    <h1>${movie.name}</h1>
+    <p>${movie.puntaje}</p>
+    <p>${movie.genero}</p>
+    <img src="${movie.img}" alt="image-movie">
+    `;
+  containerCards.innerHTML = containerCards.innerHTML + cards;
+}
+
+getMovie().then((data) => {
+  data.forEach((element) => {
+    createCard(element);
+  });
+});
